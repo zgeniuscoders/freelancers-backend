6078d61bbdea061c8ab4745b96d0bcb81841c6f2
zgeniusecoders <zgeniusecoders@gmail.com>
2025-11-29 19:51:36 +0100

    feat: ajout de la relation missions dans Skill

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Skill extends Model
 {
     protected $fillable = ["name"];
+
+    public function missions()
+    {
+        return $this->belongsToMany(Mission::class);
+    }
 }
