b39851b2f7495221704774a51e045df453a94428
zgeniusecoders <zgeniusecoders@gmail.com>
2025-11-29 18:50:20 +0100

    feat(Mission.php): ajout de la relation participations

--- app/Models/Mission.php
+++ app/Models/Mission.php
@@ -26,4 +26,8 @@ class Mission extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function participations(){
+        return $this->hasMany(Participation::class);
+    }
 }
