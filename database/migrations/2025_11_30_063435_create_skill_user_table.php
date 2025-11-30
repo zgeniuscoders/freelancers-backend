7c48dafbfdf41c5809c497b4f84929e795a9a04c
zgeniusecoders <zgeniusecoders@gmail.com>
2025-11-30 07:50:20 +0100

    feat: creation migration pour la table skill_user

---
+++ database/migrations/2025_11_30_063435_create_skill_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Skill;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('skill_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Skill::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('skill_user');
+    }
+};
